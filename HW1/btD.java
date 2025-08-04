8508a2967c33bded79e65de4424a501d1c3200d8
Đỗ Mai Hằng <24022647@vnu.edu.vn>
2025-08-04 17:25:38 +0700

    update btD

--- HW1/btD.java
+++ HW1/btD.java
@@ -23,19 +23,29 @@ public class btD {
         StdDraw.clear(StdDraw.GRAY);
         StdDraw.enableDoubleBuffering();
 
-        double x = 0, y = 0, t = 0;
+        int x = 0, y = 0;
+        int len = 1, dirIndex = 0, repeat = 0;
         int steps = 0;
-        while (Math.abs(x) < n && Math.abs(y) < n) {
-            StdDraw.setPenColor(StdDraw.WHITE);
-            StdDraw.filledSquare(x, y, 0.45);
-            x = t*Math.cos(t);
-            y = t*Math.sin(t);
-            t += Math.PI/12;
-            steps++;
-            StdDraw.setPenColor(StdDraw.BLUE);
-            StdDraw.filledSquare(x, y, 0.45);
-            StdDraw.show();
-            StdDraw.pause(40);
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+        while (Math.abs(x) < n && Math.abs(y) < n) {     
+            for(int i = 0; i < len; ++i){
+                StdDraw.setPenColor(StdDraw.WHITE);
+                StdDraw.filledSquare(x, y, 0.45);
+                x += dx[dirIndex % 4];
+                y += dy[dirIndex % 4];
+                steps++;
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+            }
+            dirIndex = (dirIndex + 1) % 4;
+            repeat++;
+            if(repeat == 2){
+                repeat = 0;
+                len++;
+            }
         }
         StdOut.println("Total steps = " + steps);
     }
