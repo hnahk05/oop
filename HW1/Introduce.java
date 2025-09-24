745dc3236c06ae0ab467626ab0365304e21a7ed8
Đỗ Mai Hằng <24022647@vnu.edu.vn>
2025-09-24 11:52:33 +0700

    done A and B HW1

---
+++ HW1/Introduce.java
@@ -0,0 +1,10 @@
+public class Introduce {
+    public static void main(String[] args){
+        System.out.println("Do Mai Hang\t24022647\tIS2\thnahk05\tdomaihang05@gmail.com\n");
+        for(int i = 9; i > 0; --i){
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
