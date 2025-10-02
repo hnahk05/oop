1d3126f2e3fc0dbab9c2979a18dde86f707a6f46
Đỗ Mai Hằng <24022647@vnu.edu.vn>
2025-10-02 14:35:07 +0700

    done CharGrid HW2

--- HW2/src/main/java/CharGrid.java
+++ HW2/src/main/java/CharGrid.java
@@ -2,33 +2,65 @@
 // CharGrid encapsulates a 2-d grid of chars and supports
 // a few operations on the grid.
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CharGrid {
 	private char[][] grid;
 
-	/**
-	 * Constructs a new CharGrid with the given grid.
-	 * Does not make a copy.
-	 * @param grid
-	 */
 	public CharGrid(char[][] grid) {
-		this.grid = grid;
+        this.grid = grid;
 	}
-	
-	/**
-	 * Returns the area for the given char in the grid. (see handout).
-	 * @param ch char to look for
-	 * @return area for given char
-	 */
+
 	public int charArea(char ch) {
-		return 0; // YOUR CODE HERE
+        int minRow = grid.length;
+        int maxRow = 0;
+        int minCol = grid[0].length;
+        int maxCol = 0;
+        for (int i = 0; i < grid.length; ++i) {
+            for (int j = 0; j < grid[0].length; ++j) {
+                if (grid[i][j] == ch) {
+                    if (i < minRow) minRow = i;
+                    if (i > maxRow) maxRow = i;
+                    if (j < minCol) minCol = j;
+                    if (j > maxCol) maxCol = j;
+                }
+            }
+        }
+        int minArea = (maxRow - minRow + 1) * (maxCol - minCol + 1);
+        if (maxRow == -1) return 0;
+        return minArea;
 	}
-	
-	/**
-	 * Returns the count of '+' figures in the grid (see handout).
-	 * @return number of + in grid
-	 */
-	public int countPlus() {
-		return 0; // YOUR CODE HERE
-	}
-	
+
+    private int armLength(int r, int c, int dr, int dc, char ch) {
+        int len = 0;
+        int i = r + dr;
+        int j = c + dc;
+        while (i >= 0 && i < grid.length && j >= 0 && j < grid[i].length && grid[i][j] == ch) {
+            len++;
+            i += dr;
+            j += dc;
+        }
+        return len;
+    }
+
+    public int countPlus() {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                char ch = grid[i][j];
+                if (ch == ' ') continue;
+                int up = armLength(i, j, -1, 0, ch);
+                int down = armLength(i, j, 1, 0, ch);
+                int left = armLength(i, j, 0, -1, ch);
+                int right = armLength(i, j, 0, 1, ch);
+
+                if (up >= 1 && down >= 1 && left >= 1 && right >= 1 &&
+                    up == down && up == left && up == right) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
